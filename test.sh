bafe27f21df21f983619bac4d84a9d699460e9f2
Kyle Hotchkiss <kyle@kylehotchkiss.com>
2015-01-10 17:50:46 -0500

    make test script safe from losing files

--- test.sh
+++ test.sh
@@ -1,9 +1,13 @@
-echo "Update testing instance..."
-git checkout testing > /dev/null 2>&1
-git add --all . > /dev/null 2>&1
-git commit -m "Update testing instance..." > /dev/null 2>&1
-git push -f testing three:master > /dev/null 2>&1
+if [[ -n $( git status --porcelain ) ]]; then
+    echo "There are uncommited changes -- please commit before proceeding with testing"
+else
+    echo "Update testing instance..."
+    git checkout testing > /dev/null 2>&1
+    git add --all . > /dev/null 2>&1
+    git commit -m "Update testing instance..." > /dev/null 2>&1
+    git push -f testing three:master > /dev/null 2>&1
 
-mocha
+    mocha
 
-git checkout three > /dev/null 2>&1
+    git checkout three > /dev/null 2>&1
+fi
