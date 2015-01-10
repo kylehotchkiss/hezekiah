1b2b534c0ad05ac4cbc19c1b0584b19948e33e73
Kyle Hotchkiss <kyle@kylehotchkiss.com>
2015-01-10 17:34:42 -0500

    try deploying to the heroku

--- test.sh
+++ test.sh
@@ -2,7 +2,8 @@ echo "Update testing instance..."
 git checkout testing > /dev/null 2>&1
 git add --all . > /dev/null 2>&1
 git commit -m "Update testing instance..." > /dev/null 2>&1
-git push -f origin three > /dev/null 2>&1
+git push -f testing three:master > /dev/null 2>&1
 
+mocha
 
 git checkout three > /dev/null 2>&1
