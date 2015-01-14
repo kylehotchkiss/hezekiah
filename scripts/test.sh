59945c80d0794c179a71a9f68666557239846b41
Kyle Hotchkiss <kyle@kylehotchkiss.com>
2015-01-13 23:33:29 -0500

    hopefully write to circleci test artifats

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,14 +7,17 @@ else
     git checkout testing > /dev/null 2>&1
     git merge -X theirs three > /dev/null 2>&1
     git commit -m "Update testing instance..." > /dev/null 2>&1
+    
     echo "Uploading to Heroku..."
     git remote add testing git@heroku.com:hezekiah-testing.git > /dev/null 2>&1
     git push -f testing testing:master > /dev/null 2>&1
     git checkout three > /dev/null 2>&1
+
     echo "Resetting Database..."
     node scripts/database.create.testing.js > /dev/null 2>&1
+
     echo "Running Mocha tests..."
-    echo $CIRCLE_TEST_REPORTS
-    echo $CIRCLE_TEST_REPORTS/xunit.xml
-    UNIT_FILE=$CIRCLE_TEST_REPORTS/xunit.xml mocha -R xunit-file
+    export LOG_XUNIT=true
+    export UNIT_FILE=$CIRCLE_TEST_REPORTS/xunit.xml
+    mocha -R xunit-file
 fi
