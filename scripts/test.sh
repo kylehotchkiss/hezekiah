1716a14a680e441a51f900c25598221602e82aa8
Kyle Hotchkiss <kyle@kylehotchkiss.com>
2015-01-13 23:42:02 -0500

    trying this locally

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ else
     git checkout testing > /dev/null 2>&1
     git merge -X theirs three > /dev/null 2>&1
     git commit -m "Update testing instance..." > /dev/null 2>&1
-    
+
     echo "Uploading to Heroku..."
     git remote add testing git@heroku.com:hezekiah-testing.git > /dev/null 2>&1
     git push -f testing testing:master > /dev/null 2>&1
@@ -17,7 +17,6 @@ else
     node scripts/database.create.testing.js > /dev/null 2>&1
 
     echo "Running Mocha tests..."
-    export LOG_XUNIT=true
     export UNIT_FILE=$CIRCLE_TEST_REPORTS/xunit.xml
     mocha -R xunit-file
 fi
