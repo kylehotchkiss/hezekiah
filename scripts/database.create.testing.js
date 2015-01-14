21f90d0d90402f76711a2977949fafdd6e0c19fc
Kyle Hotchkiss <kyle@kylehotchkiss.com>
2015-01-13 22:10:43 -0500

    env debuggr

--- scripts/database.create.testing.js
+++ scripts/database.create.testing.js
@@ -1,7 +1,9 @@
+console.log("ENV:")
+console.log( process.env.NODE_ENV )
+
 if ( process.env.NODE_ENV !== "testing" ) {
     require('node-env-file')(__dirname + '/../.env.testing');
 }
-
 
 var database = require('../models');
 
