9dcae01d9043c1540a429e0acca1f0c03fe2a971
Kyle Hotchkiss <kyle@kylehotchkiss.com>
2015-01-14 21:12:09 -0500

    make siftscience optional

--- library/siftscience.js
+++ library/siftscience.js
@@ -24,49 +24,53 @@ var countryCode = function( value ) {
 };
 
 exports.report = function( donation, charge, callback ) {
-    var data = {
-        "$type": "$transaction",
-        "$currency_code": "USD",
-        "$user_id": donation.email,
-        "$amount": donation.amount * 1000000, // todo
-        "$transaction_id": charge.id, // todo
-        "$user_email": donation.email,
-        "$session_id" : donation.session,
-        "$api_key": process.env.HEZ_SIFTSCIENCE_API,
-        "$transaction_status": charge.paid ? "$success" : "$failure", // todo
-        "$billing_address": {
-            "$name": donation.name,
-            "$city": donation.addressCity,
-            "$region": donation.addressState,
-            "$zipcode": donation.addressPostal,
-            "$address_1": donation.addressStreet,
-            "$country": countryCode( donation.addressCountry ),
-        },
-        "$payment_method": {
-            "$payment_gateway": "$stripe",
-            "$payment_type": "$credit_card",
-            "$card_last4": charge.card.last4,
-            "$verification_status": charge.card.paid,
-            "$avs_result_code": charge.card.address_line1_check && charge.card.address_zip_check
-        }
-    };
+    if ( process.env.HEZ_SIFTSCIENCE_API ) {
+        var data = {
+            "$type": "$transaction",
+            "$currency_code": "USD",
+            "$user_id": donation.email,
+            "$amount": donation.amount * 1000000, // todo
+            "$transaction_id": charge.id, // todo
+            "$user_email": donation.email,
+            "$session_id" : donation.session,
+            "$api_key": process.env.HEZ_SIFTSCIENCE_API,
+            "$transaction_status": charge.paid ? "$success" : "$failure", // todo
+            "$billing_address": {
+                "$name": donation.name,
+                "$city": donation.addressCity,
+                "$region": donation.addressState,
+                "$zipcode": donation.addressPostal,
+                "$address_1": donation.addressStreet,
+                "$country": countryCode( donation.addressCountry ),
+            },
+            "$payment_method": {
+                "$payment_gateway": "$stripe",
+                "$payment_type": "$credit_card",
+                "$card_last4": charge.card.last4,
+                "$verification_status": charge.card.paid,
+                "$avs_result_code": charge.card.address_line1_check && charge.card.address_zip_check
+            }
+        };
 
-    request({
-        url: API,
-        json: true,
-        body: data,
-        method: "POST"
-    }, function( error, response, body ) {
-        if ( error || body.status !== 0 ) {
-            if ( typeof callback === "function" ) {
-                callback( true );
+        request({
+            url: API,
+            json: true,
+            body: data,
+            method: "POST"
+        }, function( error, response, body ) {
+            if ( error || body.status !== 0 ) {
+                if ( typeof callback === "function" ) {
+                    callback( true );
+                }
+            } else {
+                if ( typeof callback === "function" ) {
+                    callback();
+                }
             }
-        } else {
-            if ( typeof callback === "function" ) {
-                callback();
-            }
-        }
-    });
+        });
+    } else {
+        callback( true );
+    }
 };
 
 exports.screen = function() {
